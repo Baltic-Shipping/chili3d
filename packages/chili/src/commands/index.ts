d6b1f3f901166344b76c8c43f217b76af99f587e
vykintasmacius <maciusvykintas@gmail.com>
2025-08-20 09:55:29 +0000

    update index.ts

--- packages/chili/src/commands/index.ts
+++ packages/chili/src/commands/index.ts
@@ -1,3 +1,4 @@
+// See CHANGELOG.md for modifications (updated 2025-08-20)
 // Part of the Chili3d Project, under the AGPL-3.0 License.
 // See LICENSE file in the project root for full license information.
 
@@ -15,3 +16,5 @@ export * from "./multistepCommand";
 export * from "./redo";
 export * from "./undo";
 export * from "./workingPlane";
+export * from "./copy";
+export * from "./paste";
